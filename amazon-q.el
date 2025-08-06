8f2b847da31c80752ad84e345917b1722b51645a
Jason Zhen <15258281+JasZhe@users.noreply.github.com>
2025-08-06 18:09:41 -0400

    add some docstrings, allow editing the prompt before sending region

--- amazon-q.el
+++ amazon-q.el
@@ -33,11 +33,11 @@ Detecting tool permission promots."
 
 (defun amazon-q--start ()
   (interactive)
-  (let* (
+  (let* (;; this is so amazon q cli will send us bells if chat notifications are enabled.
+         ;; we can then use the bell character as a way to check if aamzon q is ready to accept more prompts
          (term-term-name "xterm-256color")
          (process-environment (cons "EDITOR=emacsclient" process-environment))
-         (buffer (amazon-q--get-buffer-create))
-         )
+         (buffer (amazon-q--get-buffer-create)))
     (with-current-buffer buffer
       (amazon-q-term-mode)
       (term-exec buffer "q-chat" "q" nil nil)
@@ -76,26 +76,44 @@ Detecting tool permission promots."
   (setq amazon-q-last-client-buffer (current-buffer)))
 
 (defvar amazon-q--text-to-send nil "Text to be sent in the emacsclient buffer for amazon q.")
+(defvar amazon-q--send-region-immediately nil
+  "If t, the /editor buffer will immediately be sent via `server-edit'.
+Otherwise the user will have the opportunity to edit the prompt before being submitted to amazon q.
+see: `amazon-q--send-region'")
 
 (defun amazon-q--editor-buffer-insert-region ()
+  "Intended to be used with `server-switch-hook'.
+When emascclient is invoked, this will insert the saved region from
+`amazon-q--send-region' to the emacsclient buffer.
+If `amazon-q--send-region-immediately' is set, `server-edit' will be called
+immediately.
+Otherwise, the user will have the opportunity to edit the emacsclient buffer."
   (when amazon-q--text-to-send
     (insert amazon-q--text-to-send)
     (save-buffer)
     (setq amazon-q--text-to-send nil)
-    (server-edit)
+    (if amazon-q--send-region-immediately
+       (progn
+         (message "Sending region to amazon Q immediately..")
+         (server-edit))
+      (message "Region inserted. Edit the prompt and submit when ready."))
     (display-buffer amazon-q--buffer)))
 
 (add-hook 'server-visit-hook 'amazon-q-track-client-buffer)
 (add-hook 'server-switch-hook 'amazon-q--editor-buffer-insert-region)
 
-(defun amazon-q--send-region ()
-  (interactive)
+(defun amazon-q--send-region (arg)
+  "Send the current region to amazon Q.
+With a prefix arg, edit the prompt before sending."
+  (interactive "P")
+  (setq amazon-q--send-region-immediately (not arg))
   (setq amazon-q--buffer (amazon-q--get-buffer-create))
   (setq amazon-q--text-to-send (buffer-substring-no-properties (region-beginning) (region-end)))
   (amazon-q--send "/editor"))
 
 
 (defun amazon-q--clear-context ()
+  "Runs /clear to clear amazon Q context."
   (interactive)
   (display-buffer (amazon-q--get-buffer-create))
   (amazon-q--send "/clear")
@@ -103,12 +121,14 @@ Detecting tool permission promots."
 
 
 (defun amazon-q--compact-context ()
+  "Runs /compact to compact amazon Q context."
   (interactive)
   (display-buffer (amazon-q--get-buffer-create))
   (amazon-q--send "/compact"))
 
 
 (defun amazon-q--add-file-to-context ()
+  "Prompt for a file to add to amazon Q context."
   (interactive)
   (display-buffer (amazon-q--get-buffer-create))
   (let ((file-to-add (read-file-name "Add to context: ")))
